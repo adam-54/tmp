56e0e9607b15948258f47fdd59caad3d69b659ad
adam-54 <80105679+adam-54@users.noreply.github.com>
2022-09-24 20:59:52 +0200

    Update check01.py

--- check01.py
+++ check01.py
@@ -1,6 +1,4 @@
 a01.head()
 a02 = a01['INWEST'].describe()
 print(a02)
-print("To jest bardzo długi tekst, który pojawi się w przeglądarce\n
-To jest bardzo długi tekst, który pojawi się w przeglądarce\n
-To jest bardzo długi tekst, który pojawi się w przeglądarce")
+print('To jest bardzo długi tekst, który pojawi się w przeglądarce')
