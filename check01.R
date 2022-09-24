dffa1a4abbba996bd7e742ed647cd74f317cfd86
adam-54 <80105679+adam-54@users.noreply.github.com>
2022-09-24 20:46:13 +0200

    Update check01.R

--- check01.R
+++ check01.R
@@ -2,6 +2,6 @@ head(a01)
 a<-summary(a01$INWEST)
 print(a)
 
-print("To jest bardzo długi tekst, który pojawi się w przeglądarce","\n",
-      "To jest bardzo długi tekst, który pojawi się w przeglądarce","\n",
+print("To jest bardzo długi tekst, który pojawi się w przeglądarce\n",
+      "To jest bardzo długi tekst, który pojawi się w przeglądarce",
       "To jest bardzo długi tekst, który pojawi się w przeglądarce")
